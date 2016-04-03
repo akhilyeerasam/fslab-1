1bb2a2082b6f31badb2e103636c7009c9ebc6b08
akhilyeerasam <akhilyeerasam@hotmail.com>
2016-04-03 10:45:30 +0530

    change

--- lab7/src/lab7.cpp
+++ lab7/src/lab7.cpp
@@ -6,17 +6,16 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
+#include<iostream>
 #include<fstream>
 #include<stdlib.h>
 #include<stdio.h>
 using namespace std;
 
-class namelist
+class namedlist
 {
-	public:string buffer,l1[100];
-	int i1;
-
+	public: string buffer,l1[100];
+			int i1;
 	public:
 		void load()
 		{
@@ -29,9 +28,11 @@ class namelist
 				cout<<buffer;
 				l1[i1++]=buffer;
 			}
+			cout<<"\n";
+			return;
 		}
-
-		void load1()
+		
+		void loading()
 		{
 			i1=0;
 			fstream f1;
@@ -54,39 +55,46 @@ class namelist
 				{
 					if(l[i]>l[j])
 					{
-						temp=l[i];
-						l[i]=l[j];
+						temp=l[i];		
+						l[i]=l[j];	
 						l[j]=temp;
 					}
 				}
 			}
+			for(i=0;i<n;i++)
+			 cout<<l[i]<<"  ";
 			return *l;
 		}
 
 		void cosequentialmatch(string l2[],int i2)
 		{
+			for(i=0;i<i1;i++)
 			int i=0,j=0;
+			 cout<<l1[i];
+			for(i=0;i<i2;i++)
+			 cout<<l2[i];
 			while(i!=i1-1&&j!=i2-1)
-			{
+			{		
 				if(l1[i]<l2[j])
 					i++;
 				if(l1[i]==l2[j])
 				{
 					cout<<"String matched: ";
-					cout<<l1[i]<<"\n";
+					cout<<l1[i]<<"\n";		
 					i++; j++;
 				}
 				else
 					j++;
 			}
-
 		}
 };
 
 int main()
 {
+	cout<<"test";
 	//char* fname1,fname2;
-	namelist n1,n2;
+	namedlist n1;
+	namedlist n2;
 	//cout<<"\nEnter the name of file1: ";
 	//cin>>fname1;
 	cout<<"test";
@@ -95,11 +103,11 @@ int main()
 	*n1.l1=n1.sort(n1.l1,n1.i1);
 	//cout<<"\nEnter the name of file2: ";
 	//cin>>fname2;
-	n2.load1();
+	n2.loading();
 	cout<<"test";
 	*n2.l1=n2.sort(n2.l1,n2.i1);
 	cout<<"test";
 	n1.cosequentialmatch(n2.l1,n2.i1);
-	cout<<"test";
+	cout<<"test"<<"\n";
 	return 0;
 }
