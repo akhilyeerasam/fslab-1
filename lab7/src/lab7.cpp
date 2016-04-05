012eee3f9bddb5f56749845d2dc682948acfb4ab
akhilyeerasam <akhilyeerasam@hotmail.com>
2016-04-05 06:54:13 +0530

    working

--- lab7/src/lab7.cpp
+++ lab7/src/lab7.cpp
@@ -14,52 +14,50 @@ using namespace std;
 
 class namedlist
 {
-	public: string buffer,l1[100],l2[100];
-			int i1,i2,i;
+	public: string buffer,l1[100];
+			int i1;
 	public:
 		void load()
 		{
-			i1=-1;
+			i1=0;
 			fstream f1;
-			f1.open("1.txt",ios::in);
+			f1.open("file1.txt",ios::in);
 			while(!f1.eof())
 			{
 				getline(f1,buffer);
 				//cout<<buffer;
 				l1[i1++]=buffer;
 			}
-			cout<<i1;
 			cout<<"\nloaded file: \n";
-			for(i=-1;i<i1-1;i++)
+			for(int i=0;i<i1;i++)
 				cout<<l1[i]<<"\t";
-	cout<<"\n";
+			cout<<"\n";
 			return;
 		}
 		
 		void loading()
 		{
-			i2=-1;
+			i1=0;
 			fstream f1;
-			f1.open("2.txt",ios::in);
+			f1.open("file2.txt",ios::in);
 			while(!f1.eof())
 			{
 				getline(f1,buffer);
-				//cout<<buffer;
-				l2[i2++]=buffer;
+				cout<<buffer;
+				l1[i1++]=buffer;
 			}
-			cout<<i2<<endl;
 			cout<<"\nloaded file: \n";
-			for(i=-1;i<i2-1;i++)
-				cout<<l2[i]<<"\t";
+			for(int i=0;i<i1;i++)
+				cout<<l1[i]<<"\t";
 			cout<<"\n";
 			return;
 		}
 
-		void sort1()//string *l,int n)
+		void sort(string *l,int n)
 		{
 			int i,j;
 			string temp;
-			/*for(i=0;i<n;i++)
+			for(i=0;i<n;i++)
 			{
 				for(j=i+1;j<n;j++)
 				{
@@ -71,101 +69,66 @@ class namedlist
 					}
 				}
 			}
-			cout<<"\nsorted file is:\n";
+			cout<<"\nsorted file: ";
 			for(i=0;i<n;i++)
-			 cout<<l[i]<<"\t"; */ 
-			for(i=-1;i<i1-1;i++)
-			{
-				for(j=i+1;j<i1;j++)
-				{
-					if(l1[i]>l1[j])
-					{
-						temp=l1[i];		
-						l1[i]=l1[j];	
-						l1[j]=temp;
-					}
-				}
-			}
-			cout<<"\nsorted file is:\n";
-			for(i=-1;i<i1-1;i++)
-			 cout<<l1[i]<<"\t";
-			
-		}
-		
-		void sort2()
-		{	
-			int i,j;
-			string temp;
-			for(i=-1;i<i2-1;i++)
-			{
-				for(j=i+1;j<i2;j++)
-				{
-					if(l2[i]>l2[j])
-					{
-						temp=l2[i];		
-						l2[i]=l2[j];	
-						l2[j]=temp;
-					}
-				}
-			}
-			cout<<"\nsorted file is:\n";
-			for(i=-1;i<=i2-1;i++)
-			 cout<<l2[i]<<"\t";
+				cout<<l[i]<<"\t";
+			cout<<"\n";
+			return;
 		}
 
 		void cosequentialmatch(string l2[],int i2)
 		{
-			int i,j,c=0;
-			//for(i=0;i<i1;i++)
-			 //cout<<l1[i];
-			//for(i=0;i<i2;i++)
-			 //cout<<l2[i]; 
-			i=-1; j=-1;
-			cout<<"i1 in match"<<i1;
-			cout<<"i2 in match"<<i2;
-			while(i<=i1&&j<=i2)
+			int i=1,j=1,count=0;
+			/*for(i=0;i<i1;i++)
+			 cout<<l1[i];
+			for(i=0;i<i2;i++)
+			 cout<<l2[i]; */
+			cout<<i1<<" "<<i2;
+			for(i=1;i<i1;i++)
+			 cout<<"\npos "<<i<<"element: "<<l1[i];
+			for(i=1;i<i2;i++)
+			 cout<<"\npos "<<i<<"element: "<<l2[i];
+			i=1; j=1;
+			while(i<i1&&j<i2)
 			{		
+				cout<<"test";
 				if(l1[i]<l2[j])
 					i++;
-				if((l1[i]==l2[j])&&(l1[i]!="\n"))
+				if(l1[i]==l2[j])
 				{
-					cout<<"\nString matched: ";
+					cout<<"String matched: ";
 					cout<<l1[i]<<"\n";		
-					i++; j++;
-					c++;
+					i++; j++; count++;
 				}
 				else
 					j++;
-			} 
-			/*for(i=0;i<i1;i++)
-				for(j=0;j<i2;j++)
-					if(l1[i]==l2[j])
-					{	
-						c++;
-						cout<<"\nString matched: ";
-						cout<<l1[i];		
-					}*/
-			cout<<"\nNumber of strings matched: "<<c;
+			}
+			cout<<"\nNumber of strings matched: "<<count;
+			return;
 		}
 };
 
 int main()
 {
-	//char* fname1,fname2;
+	//cout<<"test";
+	//string fname1,fname2;
+	//fname1="file1.txt";
+	//fname2="file2.txt";
 	namedlist n1;
 	namedlist n2;
 	//cout<<"\nEnter the name of file1: ";
 	//cin>>fname1;
+	//cout<<"test";
 	n1.load();
-	//n1.sort(n1.l1,n1.i1);
-	n1.sort1();
+	//cout<<"test";
+	n1.sort(n1.l1,n1.i1);
 	//cout<<"\nEnter the name of file2: ";
 	//cin>>fname2;
 	n2.loading();
-	n2.sort2();
-	//n2.sort(n2.l1,n2.i1);
 	//cout<<"test";
-	n1.cosequentialmatch(n2.l2,n2.i2);
+	n2.sort(n2.l1,n2.i1);
+	//cout<<"test";
+	n1.cosequentialmatch(n2.l1,n2.i1);
 	//cout<<"test"<<"\n";
 	return 0;
 }
